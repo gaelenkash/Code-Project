f5a166b6833d39b5481e9004b573fa601368c1fb
Wallaceness <ncw232@gmail.com>
2019-03-08 11:29:43 -0500

    Finsihed product.

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ class SinglePageApp extends React.Component{
 			searchResults: [],
 			section: "home",
 			results: [],
-			showAll: false
+			showAll: false,
+			loading: true
 		}
 	}
 
 	componentDidMount(){
 		return axios.get(`${api}${this.state.section}.json?api-key=${Key}`)
 		.then((response)=>{
-			this.setState({searchResults: response.data.results.slice(0, 10), results: response.data.results})
+			this.setState({searchResults: response.data.results.slice(0, 10), results: response.data.results, loading: false})
 		})
 	}
 
@@ -45,55 +46,60 @@ class SinglePageApp extends React.Component{
 
 	toggle(){
 		if (this.state.searchResults.length>10){
-
+			this.setState({searchResults: this.state.results.filter((term)=>{
+				return term.title.toLowerCase().indexOf(this.state.searchTerm.toLowerCase())!==-1
+			}).slice(0, 10), showAll: false})
 		}
 		else{
-			
+			this.setState({searchResults: this.state.results.filter((term)=>{
+				return term.title.toLowerCase().indexOf(this.state.searchTerm.toLowerCase())!==-1
+			}), showAll: true})
 		}
 	}
 
 	render(){
 		return (
 			<div className="container form-horizontal">
-			<h1 className="text-center">New York Times Top News</h1>
-			<div className="form-group">
-			<div className="col-sm-6 no-padding">
-			<label className="col-xs-3 control-label">Section: </label>
-			<div className="col-xs-9">
-				<select value={this.state.section} onChange={(event)=>this.changeSection(event.target.value)} className="form-control">
-					{values.map((value)=>{
-						return (
-							<option key={value}>
-							{value}
-							</option>
-						)
-					})}
-				</select>
-			</div>
+				<h1 className="text-center">New York Times Top News</h1>
+				<div className="form-group">
+					<label className="col-xs-2 control-label">Section </label>
+					<div className="col-xs-6">
+						<select value={this.state.section} onChange={(event)=>this.changeSection(event.target.value)} className="form-control">
+							{values.map((value)=>{
+								return (
+									<option key={value} value={value}>
+									{value[0].toUpperCase()+value.slice(1)}
+									</option>
+								)
+							})}
+						</select>
+					</div>
 				</div>
-				<div className="col-sm-6 no-padding">
-				<div className="col-xs-8">
-					<input type="text" value={this.state.searchTerm} onChange={(event)=>this.search(event.target.value)} className="form-control" placeholder="Search here.."/>
-				</div>
-				<div className="col-xs-4 no-padding">
-					<label className="control-label">Showing {this.state.searchResults.length}/{this.state.results.length} results</label>
-				</div>
-			</div>
+				<div className="form-group">
+					<label className="col-xs-2 control-label">Search </label>
+					<div className="col-xs-6">
+						<input type="text" value={this.state.searchTerm} onChange={(event)=>this.search(event.target.value)} className="form-control"/>
+					</div>
+					<div className="col-xs-4 no-padding">
+						<label className="control-label">Showing {this.state.searchResults.length}/{this.state.results.length} results</label>
+					</div>
 				</div>
 				<div className="row">
-					{this.state.searchResults.map((result)=>{
+					{this.state.searchResults.length?
+						this.state.searchResults.map((result, index)=>{
 						return (
-								<div key={result.create_date} className="col-xs-6 news-group">
-									<a href={result.url}>
-										<h3>{result.title}</h3>
-									</a>
-									<p>Published {new Date(result.published_date).toLocaleString()}</p>
-									<p>{result.abstract}</p>
-								</div>
+							<div key={result.create_date} className="col-sm-6 col-xs-12 news-group">
+								<a href={result.url}>
+									<h4>{result.title}</h4>
+								</a>
+								<p>Published {new Date(result.published_date).toLocaleString()}</p>
+								<p className="abstract">{result.abstract}</p>
+							</div>
 							)
-					})}
+					}):this.state.loading?
+						<h2 className="text-center">Loading...</h2>:<h2 className="text-center">No Matching Articles</h2>}
 				</div>
-				{this.state.showAll? null: <a onClick={()=>this.toggle()}>Show All Results..</a>}
+				<a className="text-center toggle" onClick={()=>this.toggle()}>{this.state.showAll?"Trim Results..":"Show All Results.."}</a>
 			</div>
 			)
 	}
